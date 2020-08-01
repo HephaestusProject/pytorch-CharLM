6818f9bad7f62ca6e6e8833e32084840d3b55fec
Yongrae Jo <dreamgonfly@gmail.com>
2020-08-01 15:47:22 +0900

    wip

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -1,18 +1,7 @@
 import pytest
 
-from src.model.ops import add, multiply, subtract
+# from src.model.ops import add, multiply, subtract
 
 
-def test_multiply():
-    result = multiply(2, 3)
-    assert result == 6
-
-
-def test_add():
-    result = add(2, 3)
-    assert result == 5
-
-
-def test_subtract():
-    result = subtract(3, 2)
-    assert result == 1
+def dummy_test():
+    assert 1 + 1 == 3
