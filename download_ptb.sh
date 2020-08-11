629ce2a1b6000c0879d23e57be8cc55fe50fe980
Yongrae Jo <dreamgonfly@gmail.com>
2020-08-12 07:57:22 +0900

    fix: Exec form error

--- download_ptb.sh
+++ download_ptb.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 mkdir -p data/ptb
 wget https://raw.githubusercontent.com/yoonkim/lstm-char-cnn/ff8e3176f0348a5e36165b5ea42033e748c31150/data/ptb/train.txt -O data/ptb/train.txt
 wget https://raw.githubusercontent.com/yoonkim/lstm-char-cnn/ff8e3176f0348a5e36165b5ea42033e748c31150/data/ptb/valid.txt -O data/ptb/valid.txt
