8682458ffe49e454fc46eb4a9b4f3b69fb543c67
Yongrae Jo <dreamgonfly@gmail.com>
2020-08-16 13:51:53 +0900

    feat: Parameterize & enhance model

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,12 +1,28 @@
 import torch
 
-from model import CharLM
+from model import CharLM, ConvSize
 
 
 def test_model_size():
     model = CharLM(
-        num_chars=50, num_words=1000, char_embedding_dim=100, word_embedding_dim=200
+        num_chars=50,
+        num_words=1000,
+        char_embedding_dim=100,
+        char_padding_idx=0,
+        char_conv_sizes=(
+            ConvSize(kernel_size=1, out_channels=25),
+            ConvSize(kernel_size=2, out_channels=50),
+            ConvSize(kernel_size=3, out_channels=75),
+            ConvSize(kernel_size=4, out_channels=100),
+            ConvSize(kernel_size=5, out_channels=125),
+            ConvSize(kernel_size=6, out_channels=150),
+        ),
+        use_batch_norm=True,
+        num_highway_layers=1,
+        hidden_dim=300,
+        dropout=0.5,
     )
     example_input = torch.randint(high=50, size=(4, 7, 10))
-    example_output, _ = model(example_input, hidden=None)
+    model.initialize_state()
+    example_output = model(example_input)
     assert example_output.size() == (4, 7, 1000)
--- train.py
+++ train.py
@@ -11,15 +11,20 @@ Options:
     --char-vocabulary-path <char-vocabulary-path>  Load char tokenizer from [type: path]
     
     --max-word-length <max-word-length>  Maximum number of chars in a word [type: int]
-    --sequence-length <sequence-length>  Number of words in a training sequence [type: int]
+    --sequence-length <sequence-length>  Number of timesteps to unroll for [type: int]
     
     --char-embedding-dim <char-embedding-dim>  For char embedding layer of model [type: int]
-    --word-embedding-dim <word-embedding-dim>  For word embedding layer of model [type: int]
+    --hidden-dim <hidden-dim>  Hidden dimension for LSTM [type: int]
+    --num-highway-layers <num-highway-layers>  [type: int]
+    --use-batch-norm  Use BatchNorm1d after character convolutions
+    --dropout <dropout>  Dropout probability for LSTM and output layer [type: float]
+
+    --gradient-clip-val <gradient-clip-val>  [type: float]
+    --lr <lr>  Learning rate [type: float]
 
     --batch-size <batch-size>  [type: int]
     --num-workers <num-workers>  Number of processes in dataloader [type: int]
     --max-epochs <max-epochs>  Training epochs [type: int]
-
 
     --run-dir <run-dir>  Prefix path for run direcotry [default: results/runs] [type: path]
     --name <name>  Experiment name. Outputs will be saved to {run-dir}/{name}/{version} [default: run] [type: path]
@@ -31,8 +36,9 @@ from pathlib import Path
 from pytorch_lightning import LightningModule, Trainer, seed_everything
 from pytorch_lightning.callbacks import LearningRateLogger, ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
-
-from dataloader import LanguageModelingDataModule
+from pytorch_lightning import Callback
+import torch
+from lightning_dataloader import LanguageModelingDataModule
 from lightning_model import LanguageModelingLightningModel
 from utils import get_next_version
 
@@ -47,7 +53,7 @@ def train(hparams: dict):
         hparams=hparams,
         num_chars=len(lm_data_module.char_tokenizer),
         num_words=len(lm_data_module.word_tokenizer),
-        pad_token_index=lm_data_module.char_tokenizer.special_token_ids["pad_token"],
+        char_pad_token_index=lm_data_module.char_tokenizer.special_token_ids["pad_token"],
     )
 
     root_dir = hparams["--run-dir"].joinpath(hparams["--name"])
@@ -56,10 +62,7 @@ def train(hparams: dict):
     version_dir.mkdir(parents=True, exist_ok=True)
 
     logger = WandbLogger(
-        name=str(hparams["--name"]),
-        save_dir=str(version_dir),
-        offline=True,
-        version=next_version,
+        name=str(hparams["--name"]), save_dir=str(version_dir), offline=True, version=next_version,
     )
 
     checkpoint_callback = ModelCheckpoint(
@@ -70,8 +73,6 @@ def train(hparams: dict):
         period=1,
     )
 
-    lr_logger = LearningRateLogger()
-
     trainer = Trainer(
         accumulate_grad_batches=1,
         amp_level="O2",
@@ -79,13 +80,13 @@ def train(hparams: dict):
         # TODO: auto_lr_find='learning_rate',
         benchmark=False,
         deterministic=True,
-        callbacks=[lr_logger],
+        callbacks=[LearningRateLogger(), StickingProgressBarCallback(hparams)],
         check_val_every_n_epoch=1,
         checkpoint_callback=checkpoint_callback,
         distributed_backend="dp",
         fast_dev_run=False,
-        gpus=None,
-        gradient_clip_val=0.0,
+        gpus=-1 if torch.cuda.is_available() else None,
+        gradient_clip_val=hparams["--gradient-clip-val"],
         log_save_interval=10,
         logger=logger,
         max_epochs=hparams["--max-epochs"],
@@ -99,3 +100,13 @@ def train(hparams: dict):
         weights_summary="top",
     )
     trainer.fit(lm_lightning_model, lm_data_module)
+
+
+class StickingProgressBarCallback(Callback):
+    def __init__(self, hparams):
+        super().__init__()
+
+        self.hparams = hparams
+
+    def on_epoch_start(self, trainer, pl_module):
+        print(" ")
