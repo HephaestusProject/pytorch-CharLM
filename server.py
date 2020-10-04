dd901ae077e366a1e04fc1f71e6466a048494ced
Yongrae Jo <dreamgonfly@gmail.com>
2020-10-04 20:43:03 +0900

    feat: Serve model

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
+from pathlib import Path
+
 from pydantic import BaseModel
 
+from predictor import Predictor
 from serving.app_factory import create_app
+
+
+predictor = Predictor.from_checkpoint(
+    checkpoint_path=Path("checkpoints/epoch=024_val_ppl=101.52542.ckpt"),
+)
 
 
 class Request(BaseModel):
@@ -12,7 +20,8 @@ class Response(BaseModel):
 
 
 def handler(request: Request) -> Response:
-    return "hi"
+    prediction = predictor.predict(input_text=request.input_text)
+    return Response(generated_text=prediction)
 
 
 app = create_app(handler, Request, Response)
