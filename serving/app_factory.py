aab43e138003195f47f0022a7d3aca8d9cfeb778
Yongrae Jo <dreamgonfly@gmail.com>
2020-10-04 21:54:26 +0900

    feat: Add readiness probe

--- serving/app_factory.py
+++ serving/app_factory.py
@@ -7,6 +7,10 @@ def create_app(handler: Callable, request_type: Any, response_type: Any):
 
     app = FastAPI()
 
+    @app.get("/hello/")
+    async def hello() -> str:
+        return "hi"
+
     @app.post("/model/")
     async def inference(request: request_type) -> response_type:
         response = handler(request)
